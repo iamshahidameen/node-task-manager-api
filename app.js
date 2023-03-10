3b971322a4488b34d5c73aabb413e871689f662a
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-11 02:48:23 +0500

    created basic server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req, res);
+  res.send('Hello World!');
+});
+
+app.listen(5000, () => {
+  console.log('app listening on port 5000!');
+});
