86be656715bb3ed52033cf3fc72a9f284545cc57
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-17 09:21:09 +0500

    store port number in var and made a general route

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(req, res);
   res.send('Hello World!');
 });
 
-app.listen(5000, () => {
-  console.log('app listening on port 5000!');
+app.get('/hello', (req, res) => {
+  res.send('Task manager App');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
